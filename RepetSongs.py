4d8a24052019d668ab725cde4be7084b501bdcf4
shir19-meet <shir19@meet.mit.edu>
2017-07-25 11:29:14 +0200

    Stopping here

--- RepetSongs.py
+++ RepetSongs.py
@@ -2,3 +2,6 @@ Python 3.5.2 (default, Nov 17 2016, 17:05:23)
 [GCC 5.4.0 20160609] on linux
 Type "copyright", "credits" or "license()" for more information.
 >>> 
+print("Row" + " your boat")
+Row your boat
+>>> print ("Genly down the stream")
